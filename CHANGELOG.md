a7f957201825daef11f604691f51f09fc7573867
Hamze <hamzezn@gmail.com>
2024-01-18 10:32:23 +0330

    initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+## [1.0.0](www.example.com) (2024-01-22)
+
+### Features
+
+- initial release
---
+++ src/docs/swaggerDef.js
@@ -0,0 +1,21 @@
+const { version } = require('../../package.json');
+const config = require('../config/config');
+
+const swaggerDef = {
+  openapi: '3.0.0',
+  info: {
+    title: 'mail-listener API documentation',
+    version,
+    license: {
+      name: 'MIT',
+      url: 'https://github.com/hamzezn/mail-listener/blob/master/LICENSE',
+    },
+  },
+  servers: [
+    {
+      url: `http://localhost:${config.port}/v1`,
+    },
+  ],
+};
+
+module.exports = swaggerDef;
